ed174d0aa92e8047a7a9f8ed96b9d2b985d5bb5b
BRUNO ALMEIDA <buninho26@gmail.com>
2025-05-20 21:01:45 +0100

    testing to host

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Import necessary dependencies and components
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import Header from './Header';
 import Menu from './Menu';
@@ -37,6 +37,7 @@ const App = () => {
             {/* Menu component with close function, only displayed if menuOpen is true */}
             {menuOpen && <Menu closeMenu={closeMenu} />}
             <main>
+            <BrowserRouter basename="/final-project"> 
               {/* Define application routes and components associated with each path */}
               <Routes>
                 <Route path="/home/" element={<Cuisine />} />
@@ -48,6 +49,7 @@ const App = () => {
                 <Route path="/about" element={<About />} />
                 <Route path="/privacy" element={<Privacy />} />
               </Routes>
+              </BrowserRouter>
             </main>
             <Footer />
             <Cookies />
