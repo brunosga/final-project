0dca71cf52adfb94813731070ecb1c3157b26857
brunosga <82525803+brunosga@users.noreply.github.com>
2025-05-20 22:11:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import necessary dependencies
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/final-project">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
